a0cbd5898dc476083a626efeebdd1224a74ae2a2
Charles <charles.korpics@ibm.com>
2021-04-08 11:27:39 -0400

    v1

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module github.com/korpx-z/gotest
 go 1.16
 
 replace zcxpoc1/greetings => ../greetings
-
-require zcxpoc1/greetings v0.0.0-00010101000000-000000000000
--- hello.go
+++ hello.go
@@ -2,12 +2,10 @@ package main
 
 import (
     "fmt"
-
-    "zcxpoc1/greetings"
 )
 
 func main() {
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+    message := "Hello!"
     fmt.Println(message)
 }
